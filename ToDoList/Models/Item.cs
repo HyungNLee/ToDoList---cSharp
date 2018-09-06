eb3bc43b94d2bff693631aae32d648ce2b5ac3b9
Hyung Lee <hyungnaelee@gmail.com>
2018-09-06 09:37:11 -0700

    Add program and flow classes to run program in console.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace ToDoList.Models
@@ -31,6 +32,50 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Welcome to the To-Do List program.");
+      Flow();
+    }
+
+    public static void Flow()
+    {
+      Console.WriteLine("Would you like to add an item or view your list? (Add/View/Quit)");
+      string input = Console.ReadLine();
+      if (input == "Add" || input == "add")
+      {
+        Console.WriteLine("Please enter a description for your item.");
+        string description = Console.ReadLine();
+        Item newItem = new Item(description);
+        newItem.Save();
+        Console.WriteLine(newItem.GetDescription() + " has been added to your list.");
+        Flow();
+      }
+      else if (input == "View" || input == "view")
+      {
+        List<Item> viewList = Item.GetAll();
+        int number = 1;
+        foreach (Item todo in viewList)
+        {
+          Console.WriteLine(number + ". " + todo.GetDescription());
+          number++;
+        }
+        Flow();
+      }
+      else if (input == "Quit" || input == "quit")
+      {
+        Console.WriteLine("Goodbye.");
+      }
+      else
+      {
+        Console.WriteLine("Please enter a valid option.");
+        Flow();
+      }
     }
   }
 }
