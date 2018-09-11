2ae89907dde91c2c085290e52a57274f38015367
Hyung Lee <hyungnaelee@gmail.com>
2018-09-10 21:00:58 -0700

    Follow homework. Lots of bugs, doesn't build.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -35,47 +35,47 @@ namespace ToDoList.Models
     }
   }
 
-  class Program
-  {
-    static void Main()
-    {
-      Console.WriteLine("Welcome to the To-Do List program.");
-      Flow();
-    }
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("Welcome to the To-Do List program.");
+//       Flow();
+//     }
 
-    public static void Flow()
-    {
-      Console.WriteLine("Would you like to add an item or view your list? (Add/View/Quit)");
-      string input = Console.ReadLine();
-      if (input == "Add" || input == "add")
-      {
-        Console.WriteLine("Please enter a description for your item.");
-        string description = Console.ReadLine();
-        Item newItem = new Item(description);
-        newItem.Save();
-        Console.WriteLine(newItem.GetDescription() + " has been added to your list.");
-        Flow();
-      }
-      else if (input == "View" || input == "view")
-      {
-        List<Item> viewList = Item.GetAll();
-        int number = 1;
-        foreach (Item todo in viewList)
-        {
-          Console.WriteLine(number + ". " + todo.GetDescription());
-          number++;
-        }
-        Flow();
-      }
-      else if (input == "Quit" || input == "quit")
-      {
-        Console.WriteLine("Goodbye.");
-      }
-      else
-      {
-        Console.WriteLine("Please enter a valid option.");
-        Flow();
-      }
-    }
-  }
+//     public static void Flow()
+//     {
+//       Console.WriteLine("Would you like to add an item or view your list? (Add/View/Quit)");
+//       string input = Console.ReadLine();
+//       if (input == "Add" || input == "add")
+//       {
+//         Console.WriteLine("Please enter a description for your item.");
+//         string description = Console.ReadLine();
+//         Item newItem = new Item(description);
+//         newItem.Save();
+//         Console.WriteLine(newItem.GetDescription() + " has been added to your list.");
+//         Flow();
+//       }
+//       else if (input == "View" || input == "view")
+//       {
+//         List<Item> viewList = Item.GetAll();
+//         int number = 1;
+//         foreach (Item todo in viewList)
+//         {
+//           Console.WriteLine(number + ". " + todo.GetDescription());
+//           number++;
+//         }
+//         Flow();
+//       }
+//       else if (input == "Quit" || input == "quit")
+//       {
+//         Console.WriteLine("Goodbye.");
+//       }
+//       else
+//       {
+//         Console.WriteLine("Please enter a valid option.");
+//         Flow();
+//       }
+//     }
+//   }
 }
